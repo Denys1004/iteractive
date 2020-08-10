1492145e183ada370258cbe7d2099acf97ff20be
denys krychevskyi <krychevskyidenys@gmail.com>
2020-08-09 19:41:08 -0700

    text post field added

--- interactive/app_one/static/js/main.js
+++ interactive/app_one/static/js/main.js
@@ -1,15 +1,24 @@
 // Show Form for submitt post
 $('#image_post_btn').click(function(){
     $('#video_post').hide();
+    $('#text_post').hide();
     $('#image_post').show();
 })
 
 $('#video_post_btn').click(function(){
     $('#image_post').hide();
+    $('#text_post').hide();
     $('#video_post').show();
-    
 })
+
+$('#text_post_btn').click(function(){
+    $('#image_post').hide();
+    $('#video_post').hide();
+    $('#text_post').show();
+})
+
 $('#cancel_post_btn').click(function(){
     $('#image_post').hide();
+    $('#text_post').hide();
     $('#video_post').hide();
 })
